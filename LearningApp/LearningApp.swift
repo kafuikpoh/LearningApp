768534e5c294fe9172f98205c6b5b30d257fff90
Kafui Kpoh <qaphuikpoh@gmail.com>
2023-03-17 16:34:10 +0000

    Initial Commit

---
+++ LearningApp/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Kafui Kpoh on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
